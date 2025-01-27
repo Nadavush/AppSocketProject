6f350b7c7815fd673c1ed30c43b24be63e0e7e7f
Nadavush <ndvgill@gmail.com>
2025-01-27 17:22:34 +0200

    linked project to mongodb so it will remember users and messages. Added /history to find out the recent messages

--- server.py
+++ server.py
@@ -2,11 +2,19 @@ import socket
 import threading
 import rsa
 import json
+from pymongo import MongoClient
+from datetime import datetime
 
 HOST = '127.0.0.1'
 PORT = 1234
 LISTENER_LIMIT = 5
 active_clients = []
+
+MONGO_URI = "mongodb+srv://ndvgill:NadavPass@messangerapp.75ujm.mongodb.net/"
+mclient = MongoClient(MONGO_URI)
+db = mclient["MessangerApp"]
+messages_collection = db["messages"]
+users_collection = db["users"]
 
 (server_public_key, server_private_key) = rsa.newkeys(1024)
 
@@ -18,6 +26,9 @@ def client_handler(client):
         if encrypted_username:
             username = rsa.decrypt(encrypted_username, server_private_key).decode('utf-8')
             client_public_key = rsa.PublicKey.load_pkcs1(client_public_key_pem)
+            if not users_collection.find_one({"username":username}): 
+                users_collection.insert_one({"username": username,
+                                                "public_key": client_public_key.save_pkcs1().decode('utf-8')})
             active_clients.append((username, client, client_public_key))
             prompt_message = {"username": "SERVER", "message": f"{username} has joined the chat"}
             send_messages_to_clients(json.dumps(prompt_message), active_clients)
@@ -32,10 +43,20 @@ def listen_for_messages(client):
         if encrypted_message:
             try:
                 decrypted_message = rsa.decrypt(encrypted_message, server_private_key).decode('utf-8')
-                if(json.loads(decrypted_message)["message"].startswith("/whisper")==False):
+                message_data = json.loads(decrypted_message)
+                if message_data["message"].startswith("/history"):
+                    username = message_data["username"]
+                    history = messages_collection.find().sort("timestamp", -1).limit(3)
+                    history_string = "\n".join([f"{msg['username']}: {msg['message']}" for msg in history])
+                    send_message_to_client(
+                        client,
+                        json.dumps({"username": "SERVER", "message": history_string}),
+                        get_client_by_username(username)[2]
+                    )
+                elif(message_data["message"].startswith("/whisper")==False):
                     send_messages_to_clients(decrypted_message, active_clients)
                 else:
-                    whisper_message = json.loads(decrypted_message)
+                    whisper_message = message_data
                     whisper_targets = whisper_message["message"].split(" ")[1]+","+whisper_message["username"]
                     whisper_message["message"] = whisper_message["message"].split(" ", 2)[2]
                     dest_clients = []
@@ -56,6 +77,10 @@ def send_message_to_client(client, message, client_public_key):
     client.sendall(encrypted_message)
            
 def send_messages_to_clients(message, clients):
+    message_data = json.loads(message)
+    username = message_data["username"]
+    content = message_data["message"]
+    messages_collection.insert_one({"username": username, "message": content, "timestamp": datetime.now().isoformat()})
     for user in clients:
         send_message_to_client(user[1], message, user[2])
 
